4d72c0f61cb230525b6966b332bb6ef23d860891
manjunathmane <manjunathmane38@gmail.com>
2025-10-06 15:52:23 +0530

    Update simpint.py

--- simpint.py
+++ simpint.py
@@ -2,7 +2,7 @@ P=int(input("Enter the principal:"))
 R=int(input("Enter the Rate of interest:"))
 T=int(input("Enter the time:"))
 SI=(P*R*T)/100
-print("Simple interest is: "{SI})
+print("Simple interest is: ",{SI})
 amount = P*(1+R/100)**T
 CI = amount - P
-print("Compound interest is: "{CI})
+print("Compound interest is: ",{CI})
